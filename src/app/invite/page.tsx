02a7a7846c70f9948470ac8f5f12ad494820b04a
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-02-18 20:30:18 -0300

    adicionando sessão Ranking de indicações

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -3,6 +3,9 @@ import { InputField, InputIcon, InputRoot } from '@/components/input';
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react';
 import Image from 'next/image';
 import logo from '../../assets/Logo.svg';
+import cooper from '../../assets/Medal-cooper.svg';
+import gold from '../../assets/Medal-gold.svg';
+import silver from '../../assets/Medal-silver.svg';
 
 export default function Invite() {
   return (
@@ -68,7 +71,7 @@ export default function Invite() {
 
             <div className="relative bg-gray-700 border-gray-600 px-4 py-7 flex flex-col items-center justify-center rounded-xl">
               <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3
+                3°
               </span>
               <span className="text-sm text-gray-300 leading-none text-center">
                 Posição no ranking
@@ -77,6 +80,45 @@ export default function Invite() {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Ranking de indicações
+        </h2>
+
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-5 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className='font-semibold'>1°</span> | André Souza
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1030
+          </span>
+
+          <Image src={gold} alt='gold' className='absolute top-0 right-8'/>
+        </div>
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-5 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+          <span className='font-semibold'>2°</span> | André Souza
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            928
+          </span>
+
+          <Image src={silver} alt='gold' className='absolute top-0 right-8'/>
+        </div>
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-5 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+          <span className='font-semibold'>3°</span> | André Souza
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            875
+          </span>
+
+          <Image src={cooper} alt='gold' className='absolute top-0 right-8'/>
+        </div>
+      </div>
       </div>
     </div>
   );
