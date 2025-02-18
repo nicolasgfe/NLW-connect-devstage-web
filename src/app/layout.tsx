57ee6564ef687c52a016a194be03918baadf057d
Nicolas G. Fernandes <nicolasgfe3@gmail.com>
2025-02-18 20:01:31 -0300

    Adicionando sessão indique e ganhe.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
-import "./globals.css"
+import './globals.css';
 
-import type { Metadata } from "next";
-import { Montserrat, Oxanium } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Montserrat, Oxanium } from 'next/font/google';
 export const metadata: Metadata = {
-  title: "devstage",
+  title: 'devstage',
 };
 const oxanium = Oxanium({
-  weight: ['500','600'],
+  weight: ['500', '600'],
   subsets: ['latin'],
-  variable: '--font-oxanium'
-})
+  variable: '--font-oxanium',
+});
 const montserrat = Montserrat({
-  weight: ['400','600'],
+  weight: ['400', '600'],
   subsets: ['latin'],
-  variable: '--font-montserrat'
-})
-
+  variable: '--font-montserrat',
+});
 
 export default function RootLayout({
   children,
@@ -25,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
       <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/Background.png)] bg-no-repeat bg-top md:bg-right-top">
-        {children}
+        <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import logo from '../assets/Logo.svg';
 
 export default function Home() {
   return (
-    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+    
       <div className="min-h-dvh flex flex-col justify-center gap-16">
         <div className="flex flex-col gap-8 items-center md:items-start">
           <Image src={logo} alt="devstage" width={108.5} height={30} />
@@ -65,6 +65,6 @@ export default function Home() {
           </form>
         </div>
       </div>
-    </main>
+    
   );
 }
